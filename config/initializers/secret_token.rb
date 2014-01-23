415f54cda868f558bf37c0168f6d1882cac80b78
dm103 <dmce103@gmail.com>
2014-01-23 19:11:55 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bps::Application.config.secret_key_base = '96b35b057adff7922bcb9586ae9c20ab96846775440bb1d5a20e05d98077706a1d36ea3e81d93a1b7659a4a53d2f14e681acbf2758e4835ddb60e09dbbce1bd1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bps::Application.config.session_store :cookie_store, key: '_bps_session'
